955b61931c52a1cfa4f07844d5423017832ffbd1
numat <numat@stu.otc.school.nz>
2021-09-13 15:08:36 +1200

    Start of scoring, no one cares about 'resources'

--- Blackjack.py
+++ Blackjack.py
@@ -8,73 +8,84 @@ def clear():
     os.system("cls")
 
 def Pause(p):
-    time.sleep(p)
+    print("PAUSE",p)
+    #time.sleep(p)
 
 def Menu():
-    print("Main Menu \n"
-          "\n"
-          "Game: \n"
-          " 1. Rules [WIP]\n"
-          " 2. Tutorial [WIP] \n"
-          " 3. Player vs Computer [WIP]\n"
-          " 4. Player vs Player vs Computer [WIP] \n"
-        "\nCode: [WIP]\n"
-          " 5. Resources [WIP]\n"
-          " 6. Other Notes [WIP]\n"
-        "\n 7. EXIT")
+    global players
+    while True:
+        clear()
+        print("Game: \n"
+              " 1. Rules [WIP] \n"
+              " 2. Tutorial [WIP] \n"
+              " 3. Player vs Computer [WIP] \n"
+              " 4. Player vs Player vs Computer [WIP] \n"
+            "\nCode: [WIP] \n"
+              " 5. Resources [WIP]\n"
+              " 6. Other Notes [WIP]\n"
+            "\n 7. EXIT")
 
+        while True:
+            try:
+                MenuSelect = int(input("Please input a corresponding number: "))
+                if MenuSelect in range(0, 8):
+                    break
+                else:
+                    print("Invalid Input")
+                    Pause(p=1)
+            except:
+                print("Invalid Input")
+                Pause(p=1)
 
+        if MenuSelect == 1: # Rules
+            clear()
+            print("TBD")
+            input("Press [ENTER] to continue")
 
+        if MenuSelect == 2: # Tutorial
+            clear()
+            print("TBD")
+            input("Press [ENTER] to continue")
 
+        if MenuSelect == 3: # P Vs C
+            clear()
+            print("Initiating Game with 1 player")
+            Pause(p=1)
+            return 1
+
+        if MenuSelect == 4: # P Vs P Vs C
+            clear()
+            print("Initiating Game with 2 players")
+            Pause(p=1)
+            return 2
+
+        if MenuSelect == 5: # resources
+            clear()
+            print("TBD")
+            input("Press [ENTER] to continue")
+
+        if MenuSelect == 6: # other notes
+            clear()
+            print("TBD")
+            input("Press [ENTER] to continue")
+
+        if MenuSelect == 7: # exit
+            clear()
+            print("Goodbye")
+            Pause(p=1)
+            quit()
+
+def Game():
+    clear()
+    print("TBD")
+
+def Score():
+    clear()
+    print("TBD")
 
 while True:
-    Menu()
+    Players = Menu()
+    Game(Players=Players)
+    Score()
     input("END")
 
-
-while True:
-    try:
-        MenuSelect = int(input("Please input a corresponding number: "))
-
-        if MenuSelect < 1:
-            print("Invalid Input")
-            time.sleep(1)
-            continue
-        if MenuSelect >= 7:
-            print("Invalid Input")
-            time.sleep(1)
-            continue
-        if MenuSelect <= 6:
-            break
-    except ValueError:
-        print("Invalid Input")
-        time.sleep(1)
-        continue
-
-
-#____________________________________Main Menu____________________________________
-
-if MenuSelect == 1:
-    clear()
-    time.sleep(1)
-    print("Welcome to Blackjack, the rules are simple: \n"
-          "Rules... \n")
-
-
-elif MenuSelect == 2:
-    print(MenuSelect)
-
-elif MenuSelect == 3:
-    print(MenuSelect)
-
-elif MenuSelect == 4:
-    print(MenuSelect)
-
-elif MenuSelect == 5:
-    print(MenuSelect)
-
-elif MenuSelect == 6:
-    print(MenuSelect)
-
-
-input()
